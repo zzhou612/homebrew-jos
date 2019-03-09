7cf807449280fc6aff610f09e90d2a6d4d57f9d7
Nathan Zhou <zzhou612@gmail.com>
2019-03-09 08:46:18 +0800

    formula(binutils): update GNU binutils version

--- Formula/i386-jos-elf-binutils.rb
+++ Formula/i386-jos-elf-binutils.rb
@@ -1,10 +1,10 @@
 class I386JosElfBinutils < Formula
 
-  desc "FSF Binutils for i386-elf development"
+  desc "FSF Binutils for i386-elf cross development"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "https://ftpmirror.gnu.org/binutils/binutils-2.27.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz"
-  sha256 "26253bf0f360ceeba1d9ab6965c57c6a48a01a8343382130d1ed47c468a3094f"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.gz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.32.tar.gz"
+  sha256 "9b0d97b3d30df184d302bced12f976aa1e5fbf4b0be696cdebc6cca30411a46e"
 
   def install
     args = [
